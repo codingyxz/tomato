56a8fb157167d964e19c322c9bee4c264d60d596
zhxy <zxya9527@163.com>
2022-08-03 16:18:56 +0800

    add tpool

---
+++ tpool-demo/src/main/java/com/tomato/pool/TpoolDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoolDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpoolDemoApplication.class, args);
+    }
+
+}
---
+++ tpool-demo/src/test/java/com/tomato/pool/TpoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoolDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

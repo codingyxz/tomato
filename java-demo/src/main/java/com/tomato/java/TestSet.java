0ce1a05e24b140a8300c6b3385e375211e51cf55
zhxy <zxya9527@163.com>
2023-03-01 17:27:43 +0800

    test stack

--- java-demo/src/main/java/com/tomato/java/TestSet.java
+++ java-demo/src/main/java/com/tomato/java/TestSet.java
@@ -1,28 +1,42 @@
 package com.tomato.java;
 
-import java.util.HashSet;
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author zhxy
  * @Date 2023/2/15 1:44 下午
  * @Version V1.0
  **/
+
+@Slf4j
 public class TestSet {
 
 
     public static void main(String[] args) {
 
+        List<String> list = new ArrayList<>();
+        String str = "111;";
+        String[] split = str.split(";");
+        int i = 0;
+        while (i < 1000000) {
+            try {
+                System.out.println(split[1]);
+            } catch (Exception ex) {
+                log.error("---{}--{}", i, ex.getMessage(), ex);
+                if(ex.getMessage() == null){
+                    break;
+                }
+            }
+            i++;
+        }
 
-        Set<String> set = new HashSet<>();
-//        set.add("abc");
-        set.add("123");
-
-        set.remove("abc");
-        set.remove("dfd");
-
-        System.out.println(set.size());
-        set.forEach(item -> System.out.println(item));
+        // 41984  41984  41984  41984  41984  41984
+        // 44032
+        // 48128
+        // 43008
 
     }
 }
